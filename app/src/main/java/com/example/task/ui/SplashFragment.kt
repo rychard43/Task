8c4b1af9d2a8930d067acedb0faebc299f90d651
Rychard Souza <rychard.souza@popcode.com.br>
2024-02-28 16:26:09 -0300

    feat: criando tela de splash

---
+++ app/src/main/java/com/example/task/ui/SplashFragment.kt
@@ -0,0 +1,28 @@
+package com.example.task.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.task.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
