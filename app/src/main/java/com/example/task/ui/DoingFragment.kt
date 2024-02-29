35890a69f001ec9b6429a4b2807b3651598044e1
Rychard Souza <rychard.souza@popcode.com.br>
2024-02-29 15:12:05 -0300

    feat: criando novos fragments

---
+++ app/src/main/java/com/example/task/ui/DoingFragment.kt
@@ -0,0 +1,29 @@
+package com.example.task.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.task.R
+import com.example.task.databinding.FragmentDoingBinding
+import com.example.task.databinding.FragmentHomeBinding
+
+
+class DoingFragment : Fragment() {
+    private var _binding: FragmentDoingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDoingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/task/ui/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.task.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.task.R
+import com.example.task.databinding.FragmentHomeBinding
+import com.example.task.databinding.FragmentSplashBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
