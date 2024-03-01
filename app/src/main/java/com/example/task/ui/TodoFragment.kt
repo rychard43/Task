d4f847f314a8ad01d34ccf6e7799de77242cc126
Rychard Souza <rychard.souza@popcode.com.br>
2024-03-01 14:24:52 -0300

    feat: criando tela de form task

--- app/src/main/java/com/example/task/ui/TodoFragment.kt
+++ app/src/main/java/com/example/task/ui/TodoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.task.R
 import com.example.task.databinding.FragmentHomeBinding
 import com.example.task.databinding.FragmentTodoBinding
@@ -22,6 +23,19 @@ class TodoFragment : Fragment() {
     ): View {
         _binding = FragmentTodoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.floatingActionButtonADDTask.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
     }
 
     override fun onDestroyView() {
