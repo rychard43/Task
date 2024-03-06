8505a44ce5aa6765a0b4a34d5c3dcfcbeeb85688
Rychard Souza <rychard.souza@popcode.com.br>
2024-03-06 16:05:44 -0300

    feat: recuperando usuario

--- app/src/main/java/com/example/task/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/example/task/ui/auth/RecoverAccountFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import com.example.task.R
 import com.example.task.databinding.FragmentRecoverAccountBinding
 import com.example.task.utils.initToolbar
 import com.example.task.utils.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RecoverAccountFragment : Fragment() {
 
     private var _binding: FragmentRecoverAccountBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +31,7 @@ class RecoverAccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
         initToolbar(binding.toolbar)
         initListeners()
     }
@@ -35,7 +42,23 @@ class RecoverAccountFragment : Fragment() {
         }
     }
 
-    private fun validateData(): Boolean {
+    private fun recoverAccount(email: String) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                binding.progressBar.isVisible = false
+                if (task.isSuccessful) {
+                    showBottomSheet(message = getString(R.string.enviou_link_recuperacao))
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.erro_recuperar) + ": " + task.exception?.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    private fun validateData() {
         val email = binding.editTextEmail.text.toString()
         var isValid = true
         if (email.isEmpty()) {
@@ -43,7 +66,11 @@ class RecoverAccountFragment : Fragment() {
             showBottomSheet(message = getString(R.string.email_obrigatorio))
             isValid = false
         }
-        return isValid
+
+        if (isValid) {
+            binding.progressBar.isVisible = true
+            recoverAccount(email)
+        }
     }
 
     override fun onDestroyView() {
