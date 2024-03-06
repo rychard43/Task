9c9cbe88673f60138985ca18f9a746b8b46a2d59
Rychard Souza <rychard.souza@popcode.com.br>
2024-03-06 15:47:38 -0300

    feat: login usuario

--- app/src/main/java/com/example/task/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/task/ui/auth/LoginFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.task.R
 import com.example.task.databinding.FragmentLoginBinding
 import com.example.task.utils.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +31,7 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        auth = Firebase.auth
         initListeners()
     }
 
@@ -34,6 +40,23 @@ class LoginFragment : Fragment() {
         binding.buttonRecoverAccount.setOnClickListener { goToRecoverAccount() }
         binding.buttonLogin.setOnClickListener { goToHome() }
     }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_global_homeFragment)
+                } else {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.erro_logar) + ": " + task.exception?.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
 
     private fun goToCreateAccount() {
         findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
@@ -44,12 +67,10 @@ class LoginFragment : Fragment() {
     }
 
     private fun goToHome() {
-        if (validateData()) {
-            findNavController().navigate(R.id.action_global_homeFragment)
-        }
+        validateData()
     }
 
-    private fun validateData(): Boolean {
+    private fun validateData() {
         val email = binding.editTextEmail.text.toString()
         val password = binding.editTextPassword.text.toString()
 
@@ -66,7 +87,10 @@ class LoginFragment : Fragment() {
             isValid = false
         }
 
-        return isValid
+        if (isValid) {
+            binding.progressBar.isVisible = true
+            login(email, password)
+        }
     }
 
     override fun onDestroyView() {
